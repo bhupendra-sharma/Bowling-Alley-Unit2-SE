204b522665d76870b9d714f2fb1fec7d95f25894
bhupendra-sharma <bhupendra.s@students.iit.ac.in>
2022-03-20 19:05:05 +0530

    Emoticon added

---
+++ LaneView.java
@@ -0,0 +1,231 @@
+/*
+ *  constructs a prototype Lane View
+ *
+ */
+
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+import java.util.*;
+
+public class LaneView implements EventObserver {
+	private boolean initDone = true;
+
+	private final JFrame frame;
+	private final Container cpanel;
+	private Vector bowlers;
+
+	private JLabel[][] ballLabel;
+	private JLabel[][] scoreLabel;
+
+	private final Lane lane;
+
+	private int scr;
+	public LaneView(Lane lane, int laneNum) {
+
+		this.lane = lane;
+
+		frame = new JFrame("Lane " + laneNum + ":");
+		cpanel = frame.getContentPane();
+		cpanel.setLayout(new BorderLayout());
+
+		frame.addWindowListener(new WindowAdapter() {
+			public void windowClosing(WindowEvent e) {
+				frame.setVisible(false);
+			}
+		});
+
+		cpanel.add(new JPanel());
+
+	}
+
+	public void show() {
+		frame.setVisible(true);
+	}
+
+	public void hide() {
+		frame.setVisible(false);
+	}
+
+	private JPanel makeFrame(Vector<Bowler> party) {
+
+		initDone = false;
+		bowlers = party;
+		int numBowlers = bowlers.size();
+
+		JPanel panel = new JPanel();
+
+		panel.setLayout(new GridLayout(0, 1));
+
+		JPanel[][] balls = new JPanel[numBowlers][25];
+		ballLabel = new JLabel[numBowlers][25];
+		JPanel[][] scores = new JPanel[numBowlers][11];
+		scoreLabel = new JLabel[numBowlers][11];
+		JPanel[][] ballGrid = new JPanel[numBowlers][11];
+		JPanel[] pins = new JPanel[numBowlers];
+
+		for (int i = 0; i != numBowlers; i++) {
+			for (int j = 0; j != 23; j++) {
+				ballLabel[i][j] = new JLabel(" ");
+				balls[i][j] = new JPanel();
+				balls[i][j].setBorder(
+					BorderFactory.createLineBorder(Color.BLACK));
+				balls[i][j].add(ballLabel[i][j]);
+			}
+			ballLabel[i][24] = new JLabel(" ");
+			balls[i][24] = new JPanel();
+			balls[i][24].setBorder(BorderFactory.createLineBorder(Color.BLACK));
+			balls[i][24].add(ballLabel[i][24]);
+		}
+
+		for (int i = 0; i != numBowlers; i++) {
+			for (int j = 0; j != 9; j++) {
+				ballGrid[i][j] = new JPanel();
+				ballGrid[i][j].setLayout(new GridLayout(0, 3));
+				ballGrid[i][j].add(new JLabel("  "), BorderLayout.EAST);
+				ballGrid[i][j].add(balls[i][2 * j], BorderLayout.EAST);
+				ballGrid[i][j].add(balls[i][2 * j + 1], BorderLayout.EAST);
+			}
+			int j = 9;
+			ballGrid[i][j] = new JPanel();
+			ballGrid[i][j].setLayout(new GridLayout(0, 3));
+			ballGrid[i][j].add(balls[i][2 * j]);
+			ballGrid[i][j].add(balls[i][2 * j + 1]);
+			ballGrid[i][j].add(balls[i][2 * j + 2]);
+			j = 10;
+			ballGrid[i][j] = new JPanel();
+			ballGrid[i][j].setLayout(new GridLayout(0, 1));
+			ballGrid[i][j].add(balls[i][24]);
+		}
+
+		for (int i = 0; i != numBowlers; i++) {
+			pins[i] = new JPanel();
+			pins[i].setBorder(
+				BorderFactory.createTitledBorder(
+					((Bowler) bowlers.get(i)).getNick()));
+			pins[i].setLayout(new GridLayout(0, 11));
+			for (int k = 0; k != 10; k++) {
+				scores[i][k] = new JPanel();
+				scoreLabel[i][k] = new JLabel("  ", SwingConstants.CENTER);
+				scores[i][k].setBorder(
+					BorderFactory.createLineBorder(Color.BLACK));
+				scores[i][k].setLayout(new GridLayout(0, 1));
+				scores[i][k].add(ballGrid[i][k], BorderLayout.EAST);
+				scores[i][k].add(scoreLabel[i][k], BorderLayout.SOUTH);
+				pins[i].add(scores[i][k], BorderLayout.EAST);
+			}
+			scores[i][10] = new JPanel();
+			scoreLabel[i][10] = new JLabel("  ", SwingConstants.CENTER);
+			scores[i][10].setBorder(
+					BorderFactory.createLineBorder(Color.BLACK));
+			scores[i][10].setLayout(new GridLayout(0, 1));
+			scores[i][10].add(ballGrid[i][10], BorderLayout.CENTER);
+			pins[i].add(scores[i][10], BorderLayout.EAST);
+			panel.add(pins[i]);
+		}
+
+		initDone = true;
+		return panel;
+	}
+
+	public void receiveEvent(Object eventObject) {
+	if(eventObject instanceof Lane) {
+		if (lane.isPartyAssigned()) {
+			int numBowlers = ((Lane)eventObject).getParty().size();
+			while (!initDone) {
+				//System.out.println("chillin' here.");
+				try {
+					Thread.sleep(1);
+				} catch (Exception e) {
+				}
+			}
+
+			if (((Lane)eventObject).getFrameNum() == 1
+					&& ((Lane)eventObject).getBall() == 0
+					&& ((Lane)eventObject).getIndex() == 0) {
+				System.out.println("Making the frame.");
+				cpanel.removeAll();
+				cpanel.add(makeFrame(((Lane)eventObject).getParty()), "Center");
+
+				// Button Panel
+				JPanel buttonPanel = new JPanel();
+				buttonPanel.setLayout(new FlowLayout());
+
+				JButton maintenance = new JButton("Maintenance Call");
+				JPanel maintenancePanel = new JPanel();
+				maintenancePanel.setLayout(new FlowLayout());
+				maintenance.addActionListener(new ActionListener() {
+					@Override
+					public void actionPerformed(ActionEvent actionEvent) {
+						lane.pauseGame();
+					}
+				});
+				maintenancePanel.add(maintenance);
+
+				buttonPanel.add(maintenancePanel);
+
+				cpanel.add(buttonPanel, "South");
+
+				frame.pack();
+
+			}
+
+			int[][] lescores = ((Lane)eventObject).getCumulScore();
+			for (int k = 0; k < numBowlers; k++) {
+				for (int i = 0; i <= ((Lane)eventObject).getFrameNum() - 1; i++) {
+					if (lescores[k][i] != 0)
+						scoreLabel[k][i].setText(
+								(Integer.toString(lescores[k][i])));
+					int scr;
+
+					if(i==0)
+						scr=lescores[k][i];
+					else
+						scr=lescores[k][i]-lescores[k][i-1];
+					if(scr==10)
+					ballLabel[k][24].setText("\uD83D\uDE01");
+					else if(scr<=9 && scr>=6)
+						ballLabel[k][24].setText("\uD83D\uDE0A");
+					else if(scr<=5 && scr>=3)
+						ballLabel[k][24].setText("\uD83D\uDE11");
+					else
+						ballLabel[k][24].setText("\uD83D\uDE30");
+				}
+				for (int i = 0; i < 21; i++) {
+					if (((int[]) (((Lane)eventObject).getScore())
+							.get(bowlers.get(k)))[i]
+							!= -1)
+						if (((int[]) (((Lane)eventObject).getScore())
+								.get(bowlers.get(k)))[i]
+								== 10
+								&& (i % 2 == 0 || i == 19))
+							ballLabel[k][i].setText("X");
+						else if (
+								i > 0
+										&& ((int[]) (((Lane)eventObject).getScore())
+										.get(bowlers.get(k)))[i]
+										+ ((int[]) (((Lane)eventObject).getScore())
+										.get(bowlers.get(k)))[i
+										- 1]
+										== 10
+										&& i % 2 == 1)
+							ballLabel[k][i].setText("/");
+						else if (((int[]) (((Lane)eventObject).getScore()).get(bowlers.get(k)))[i] == -2) {
+
+							ballLabel[k][i].setText("F");
+						} else
+							ballLabel[k][i].setText(
+									(Integer.toString(((int[]) (((Lane)eventObject).getScore())
+											.get(bowlers.get(k)))[i])));
+						int scr = ((int[]) (((Lane)eventObject).getScore())
+								.get(bowlers.get(k)))[i];
+
+				}
+			}
+
+		}
+	}
+	}
+
+
+}
